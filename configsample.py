b239d0ebc61fb49eef531e6fd05d79c5654470fc
Zhayria <89924086+Zhayria@users.noreply.github.com>
2025-06-15 21:09:06 -0400

    Intial commit: Mac Keylogger Project

---
+++ configsample.py
@@ -0,0 +1,6 @@
+EMAIL_ADDRESS = "your_email@example.com"
+EMAIL_PASSWORD = "your_app_password"
+EMAIL_RECEIVER = "receiver@example.com"
+SMTP_SERVER = "smtp.gmail.com"
+SMTP_PORT = 587
+FERNET_KEY = b'your_generated_key_here'
